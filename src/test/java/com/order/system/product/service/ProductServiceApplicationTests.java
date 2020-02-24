a2aa268f212c53d3219f432d40bad17418847254
Nasim salmany <nasimsalmani1993@gmail.com>
2020-02-24 09:42:34 +0100

    First commit in Product Service

---
+++ src/test/java/com/order/system/product/service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.system.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
